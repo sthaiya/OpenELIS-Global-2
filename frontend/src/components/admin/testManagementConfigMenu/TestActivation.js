608eab469eebdf8ad9f1466694230211ba970cf7
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2025-02-06 14:43:28 +0530

    feat(testManagementPage/testActivationDeactivationPage): inital ui rewritten

--- frontend/src/components/admin/testManagementConfigMenu/TestActivation.js
+++ frontend/src/components/admin/testManagementConfigMenu/TestActivation.js
@@ -22,6 +22,8 @@ import {
   Select,
   SelectItem,
   Stack,
+  Checkbox,
+  Modal,
 } from "@carbon/react";
 import {
   getFromOpenElisServer,
@@ -65,35 +67,298 @@ function TestActivation() {
 
   const componentMounted = useRef(false);
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [testActivationData, setTestActivationData] = useState({});
+  const [changedTestActivationData, setChangedTestActivationData] = useState(
+    {},
+  );
+  const [jsonChangeList, setJsonChangeList] = useState({
+    activateTest: [],
+    deactivateTest: [],
+  });
+
+  function handleTestActivationData(res) {
+    if (!res) {
+      setIsLoading(true);
+    } else {
+      setTestActivationData(res);
+      setChangedTestActivationData(res);
+    }
+  }
+
+  function testActivationPostCall() {
+    setIsLoading(true);
+    postToOpenElisServerJsonResponse(
+      `/rest/TestOrderability`,
+      JSON.stringify({
+        jsonChangeList: JSON.stringify({
+          activateTest: JSON.stringify(jsonChangeList.activateTest),
+          deactivateTest: JSON.stringify(jsonChangeList.deactivateTest),
+        }),
+      }),
+      (res) => testActivationPostCallback(res),
+    );
+  }
+
+  function testActivationPostCallback(res) {
+    if (res) {
+      setIsLoading(false);
+      addNotification({
+        title: intl.formatMessage({
+          id: "notification.title",
+        }),
+        message: intl.formatMessage({
+          id: "notification.user.post.save.success",
+        }),
+        kind: NotificationKinds.success,
+      });
+      setNotificationVisible(true);
+      setTimeout(() => {
+        window.location.reload();
+      }, 200);
+    } else {
+      addNotification({
+        kind: NotificationKinds.error,
+        title: intl.formatMessage({ id: "notification.title" }),
+        message: intl.formatMessage({ id: "server.error.msg" }),
+      });
+      setNotificationVisible(true);
+      setTimeout(() => {
+        window.location.reload();
+      }, 200);
+    }
+  }
+
+  useEffect(() => {
+    componentMounted.current = true;
+    setIsLoading(true);
+    getFromOpenElisServer(`/rest/TestActivation`, (res) => {
+      handleTestActivationData(res);
+    });
+    return () => {
+      componentMounted.current = false;
+      setIsLoading(false);
+    };
+  }, []);
+
+  if (!isLoading) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  }
+
   return (
     <>
-      {notificationVisible === true ? <AlertDialog /> : ""}
+      {notificationVisible && <AlertDialog />}
       <div className="adminPageContent">
         <PageBreadCrumb breadcrumbs={breadcrumbs} />
-        <Grid fullWidth={true}>
-          <Column lg={16} md={8} sm={4}>
-            <Section>
-              <Heading>
-                <FormattedMessage id="label.testActivate" />
-              </Heading>
-            </Section>
-          </Column>
-        </Grid>
-        <br />
-        <hr />
-        <br />
-        <Grid fullWidth={true}>
-          <Column lg={16} md={8} sm={4}>
-            <Section>
+        <div className="orderLegendBody">
+          <Grid fullWidth={true}>
+            <Column lg={16} md={8} sm={4}>
               <Section>
                 <Heading>
-                  <FormattedMessage id="instructions.test.activation" />
+                  <FormattedMessage id="label.testActivate" />
                 </Heading>
               </Section>
-            </Section>
-          </Column>
-        </Grid>
+            </Column>
+          </Grid>
+          <br />
+          <hr />
+          <br />
+          <Grid fullWidth={true}>
+            <Column lg={16} md={8} sm={4}>
+              <Section>
+                <Section>
+                  <Section>
+                    <Section>
+                      <Heading>
+                        <FormattedMessage id="instructions.test.activation" />
+                      </Heading>
+                    </Section>
+                  </Section>
+                </Section>
+              </Section>
+            </Column>
+          </Grid>
+          <br />
+          <hr />
+          <br />
+          <Grid fullWidth={true}>
+            <Column lg={16} md={8} sm={4}>
+              <Button
+                disabled={
+                  jsonChangeList?.activateTest?.length === 0 &&
+                  jsonChangeList?.deactivateTest?.length === 0
+                }
+                onClick={() => {
+                  setIsConfirmModalOpen(true);
+                }}
+                type="button"
+              >
+                <FormattedMessage id="label.button.submit" />
+              </Button>{" "}
+              <Button
+                onClick={() => window.location.assign("/admin#TestActivation")}
+                kind="tertiary"
+                type="button"
+              >
+                <FormattedMessage id="label.button.cancel" />
+              </Button>
+            </Column>
+          </Grid>
+          <br />
+          {changedTestActivationData?.activeTestList &&
+          changedTestActivationData?.activeTestList?.length > 0 ? (
+            <Grid fullWidth={true}>
+              {changedTestActivationData?.activeTestList?.map((sample) => (
+                <>
+                  <Column lg={16} md={8} sm={4} key={sample.sampleType.id}>
+                    <Section>
+                      <Section>
+                        <Section>
+                          <Heading>{sample.sampleType.value}</Heading>
+                        </Section>
+                      </Section>
+                    </Section>
+                  </Column>
+                  {sample.activeTests.map((test) => (
+                    <Column
+                      lg={4}
+                      md={4}
+                      sm={4}
+                      key={`${sample.sampleType.id}-${test.id}`}
+                    >
+                      <Checkbox
+                        id={`${sample.sampleType.id}-${test.id}`}
+                        labelText={test.value}
+                        checked={true}
+                        onChange={(_, { checked }) => {
+                          // handleActiveTestsCheckboxChange(
+                          //   test,
+                          //   sample.sampleType.id,
+                          //   checked,
+                          // );
+                        }}
+                      />
+                    </Column>
+                  ))}
+                  {sample.inactiveTests.map((test) => (
+                    <Column
+                      lg={4}
+                      md={4}
+                      sm={4}
+                      key={`${sample.sampleType.id}-${test.id}`}
+                    >
+                      <Checkbox
+                        id={`${sample.sampleType.id}-${test.id}`}
+                        labelText={test.value}
+                        checked={false}
+                        onChange={(_, { checked }) => {
+                          // handleInactiveTestsCheckboxChange(
+                          //   test,
+                          //   sample.sampleType.id,
+                          //   checked,
+                          // );
+                        }}
+                      />
+                    </Column>
+                  ))}
+                </>
+              ))}
+            </Grid>
+          ) : (
+            <></>
+          )}
+          <br />
+          <hr />
+          <br />
+          {changedTestActivationData?.inactiveTestList &&
+          changedTestActivationData?.inactiveTestList?.length > 0 ? (
+            <Grid fullWidth={true}>
+              {changedTestActivationData.inactiveTestList.map((sample) => (
+                <>
+                  <Column lg={16} md={8} sm={4} key={sample.sampleType.id}>
+                    <Section>
+                      <Section>
+                        <Section>
+                          <Heading>{sample.sampleType.value}</Heading>
+                        </Section>
+                      </Section>
+                    </Section>
+                  </Column>
+                  {sample.activeTests.map((test) => (
+                    <Column
+                      lg={4}
+                      md={4}
+                      sm={4}
+                      key={`${sample.sampleType.id}-${test.id}`}
+                    >
+                      <Checkbox
+                        id={`${sample.sampleType.id}-${test.id}`}
+                        labelText={test.value}
+                        checked={true}
+                        onChange={(_, { checked }) => {
+                          // handleActiveTestsCheckboxChange(
+                          //   test,
+                          //   sample.sampleType.id,
+                          //   checked,
+                          // );
+                        }}
+                      />
+                    </Column>
+                  ))}
+                  {sample.inactiveTests.map((test) => (
+                    <Column
+                      lg={4}
+                      md={4}
+                      sm={4}
+                      key={`${sample.sampleType.id}-${test.id}`}
+                    >
+                      <Checkbox
+                        id={`${sample.sampleType.id}-${test.id}`}
+                        labelText={test.value}
+                        checked={false}
+                        onChange={(_, { checked }) => {
+                          // handleInactiveTestsCheckboxChange(
+                          //   test,
+                          //   sample.sampleType.id,
+                          //   checked,
+                          // );
+                        }}
+                      />
+                    </Column>
+                  ))}
+                </>
+              ))}
+            </Grid>
+          ) : (
+            <></>
+          )}
+          <br />
+        </div>
       </div>
+
+      <Modal
+        open={isConfirmModalOpen}
+        size="md"
+        modalHeading={<FormattedMessage id="label.test.order.confirm" />}
+        primaryButtonText={<FormattedMessage id="column.name.accept" />}
+        secondaryButtonText={<FormattedMessage id="back.action.button" />}
+        onRequestSubmit={() => {
+          setIsConfirmModalOpen(false);
+          testActivationPostCall();
+        }}
+        onRequestClose={() => {
+          setIsConfirmModalOpen(false);
+        }}
+      >
+        <Grid fullWidth={true}>
+          <Column lg={16} md={8} sm={4}></Column>
+        </Grid>
+      </Modal>
     </>
   );
 }
