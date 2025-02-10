1d6b7c15fd8dc1a015027c3f8f1cc565634c195e
Vaishnavi Bhandari <vaishnavibhandari.128@gmail.com>
2025-02-10 16:10:28 +0000

    Updated
    Signed-off-by: Vaishnavi Bhandari <vaishnavibhandari.128@gmail.com>

--- frontend/src/components/admin/ProviderMenu/ProviderMenu.js
+++ frontend/src/components/admin/ProviderMenu/ProviderMenu.js
@@ -449,6 +449,12 @@ function ProviderMenu() {
             value={telephone}
             onChange={(e) => setTelephone(e.target.value)}
           />
+          <TextInput
+            id="fax"
+            labelText={intl.formatMessage({ id: "provider.fax" })}
+            value={fax}
+            onChange={(e) => setFax(e.target.value)}
+          />
           <Dropdown
             id="isActive"
             titleText="Active"
@@ -457,12 +463,6 @@ function ProviderMenu() {
             itemToString={(item) => (item ? item.value : "")}
             selectedItem={isActive}
             onChange={({ selectedItem }) => setIsActive(selectedItem)}
-          />
-          <TextInput
-            id="fax"
-            labelText={intl.formatMessage({ id: "provider.fax" })}
-            value={fax}
-            onChange={(e) => setFax(e.target.value)}
           />
         </Modal>
 
