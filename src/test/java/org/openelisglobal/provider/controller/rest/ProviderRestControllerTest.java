104d56a21dbbd96cae6a8548c6185cff3817788c
Isabirye1515 <isabiryeelijah15@gmail.com>
2025-02-28 11:08:43 +0300

    added getPerson by id test

--- src/test/java/org/openelisglobal/provider/controller/rest/ProviderRestControllerTest.java
+++ src/test/java/org/openelisglobal/provider/controller/rest/ProviderRestControllerTest.java
@@ -29,20 +29,34 @@ public class ProviderRestControllerTest extends BaseWebContextSensitiveTest {
         assertEquals(200, status);
 
         String providerJson = urlResult.getResponse().getContentAsString();
-        System.out.println("Provider Response: " + providerJson); // Debugging output
 
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, Object> providerMap = objectMapper.readValue(providerJson,
                 new TypeReference<Map<String, Object>>() {
                 });
 
-        // Assertions on the map values
         assertEquals("2", providerMap.get("id"));
         assertEquals("1234567891", providerMap.get("npi"));
         assertEquals("EXT123466", providerMap.get("externalId"));
         assertEquals("S", providerMap.get("providerType"));
         assertEquals(false, providerMap.get("active"));
         assertEquals(true, providerMap.get("desynchronized"));
+    }
+
+    @Test
+    public void getPerson_shouldReturnPersonInfoByID() throws Exception {
+        MvcResult urlResult = super.mockMvc.perform(get("/rest/Provider/Person/2")
+                .accept(MediaType.APPLICATION_JSON_VALUE).contentType(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        String personJson = urlResult.getResponse().getContentAsString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> personInfo = objectMapper.readValue(personJson, new TypeReference<Map<String, Object>>() {
+        });
+
+        assertEquals("James", personInfo.get("firstName"));
+        assertEquals("Mark", personInfo.get("middleName"));
+        assertEquals("Orion", personInfo.get("city"));
+
     }
 
 }
