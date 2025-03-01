89e3a2a2ae37957754d9c891dd9e2d8149d9a83d
Isabirye1515 <isabiryeelijah15@gmail.com>
2025-03-01 23:38:11 +0300

    added the providerMenu rest controller test

--- src/main/java/org/openelisglobal/provider/controller/rest/ProviderMenuRestController.java
+++ src/main/java/org/openelisglobal/provider/controller/rest/ProviderMenuRestController.java
@@ -158,7 +158,7 @@ public class ProviderMenuRestController extends BaseMenuController<Provider> {
     }
 
     // gnr: Deactivate not Delete
-    @PostMapping(value = "/DeleteProvider", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/DeleteProvider", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> showDeleteProvider(HttpServletRequest request,
             @RequestParam(value = ID, required = false) @Pattern(regexp = "[a-zA-Z0-9 -]*") String id,
             @Valid @ModelAttribute("form") ProviderMenuForm form, BindingResult result)
