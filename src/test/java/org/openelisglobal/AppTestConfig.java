7b380e32fe0076367c90812c2b02f93ad7ed6216
Adarsh Samani <samaniadarsh@gmail.com>
2025-03-20 01:40:52 +0530

    Modify AppTestConfig

--- src/test/java/org/openelisglobal/AppTestConfig.java
+++ src/test/java/org/openelisglobal/AppTestConfig.java
@@ -75,8 +75,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "org.openelisglobal.panel", "org.openelisglobal.panelitem", "org.openelisglobal.reports",
         "org.openelisglobal.userrole", "org.openelisglobal.unitofmeasure", "org.openelisglobal.testtrailer",
         "org.openelisglobal.scriptlet", "org.openelisglobal.localization", "org.openelisglobal.systemuser",
-        "org.openelisglobal.systemmodule", "org.openelisglobal.testdictionary",
-        "org.openelisglobal.dictionarycategory", "org.openelisglobal.testanalyte" }, excludeFilters = {
+        "org.openelisglobal.systemmodule", "org.openelisglobal.testdictionary", "org.openelisglobal.dictionarycategory",
+        "org.openelisglobal.testanalyte" }, excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.patient.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.organization.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.sample.controller.*"),
