f92a131d3fcc79cb8fc2b42400c5f0c0fc12782a
Adarsh Samani <samaniadarsh@gmail.com>
2025-03-21 20:51:19 +0530

    Modify the tests to rectify checkFormat and apply format

--- src/test/java/org/openelisglobal/patientidentity/PatientIdentityServiceTest.java
+++ src/test/java/org/openelisglobal/patientidentity/PatientIdentityServiceTest.java
@@ -47,16 +47,17 @@ public class PatientIdentityServiceTest extends BaseWebContextSensitiveTest {
     public void get_shouldReturnPatientIdentityWithGivenId() {
         PatientIdentity patientIdentity = patientIdentityService.get("1");
         assertNotNull(patientIdentity);
-        assertEquals("AB123456789", patientIdentity.getIdentityData());
+        assertEquals("334-422-A", patientIdentity.getIdentityData());
         assertEquals("1", patientIdentity.getPatientId());
-        assertEquals("1", patientIdentity.getIdentityTypeId());
+        assertEquals("9", patientIdentity.getIdentityTypeId());
     }
 
     @Test
     public void getPatientIdentitiesForPatient_shouldReturnAllIdentitiesForPatient() {
         List<PatientIdentity> patientIdentities = patientIdentityService.getPatientIdentitiesForPatient("1");
-        assertTrue(patientIdentities.size() == 1);
-        assertEquals("AB123456789", patientIdentities.get(0).getIdentityData());
+        // assertTrue(patientIdentities.size() == 1);
+        assertEquals(3, patientIdentities.size());
+        assertEquals("334-422-A", patientIdentities.get(0).getIdentityData());
     }
 
     @Test
@@ -67,21 +68,21 @@ public class PatientIdentityServiceTest extends BaseWebContextSensitiveTest {
 
     @Test
     public void getPatitentIdentityForPatientAndType_shouldReturnMatchingIdentity() {
-        PatientIdentity patientIdentity = patientIdentityService.getPatitentIdentityForPatientAndType("1", "1");
+        PatientIdentity patientIdentity = patientIdentityService.getPatitentIdentityForPatientAndType("1", "9");
         assertNotNull(patientIdentity);
-        assertEquals("AB123456789", patientIdentity.getIdentityData());
+        assertEquals("334-422-A", patientIdentity.getIdentityData());
     }
 
     @Test
     public void getPatitentIdentityForPatientAndType_shouldReturnNullForNonExistingMatch() {
-        PatientIdentity patientIdentity = patientIdentityService.getPatitentIdentityForPatientAndType("1", "2");
+        PatientIdentity patientIdentity = patientIdentityService.getPatitentIdentityForPatientAndType("1", "5");
         assertEquals(null, patientIdentity);
     }
 
     @Test
     public void getPatientIdentitiesByValueAndType_shouldReturnMatchingIdentities() {
-        List<PatientIdentity> patientIdentities = patientIdentityService
-                .getPatientIdentitiesByValueAndType("AB123456789", "1");
+        List<PatientIdentity> patientIdentities = patientIdentityService.getPatientIdentitiesByValueAndType("334-422-A",
+                "9");
         assertTrue(patientIdentities.size() == 1);
         assertEquals("1", patientIdentities.get(0).getPatientId());
     }
@@ -98,8 +99,8 @@ public class PatientIdentityServiceTest extends BaseWebContextSensitiveTest {
         cleanRowsInCurrentConnection(new String[] { "patient_identity" });
         // Create a new patient identity
         PatientIdentity patientIdentity = new PatientIdentity();
-        patientIdentity.setIdentityTypeId("2");
-        patientIdentity.setPatientId("1");
+        patientIdentity.setIdentityTypeId("9");
+        patientIdentity.setPatientId("2");
         patientIdentity.setIdentityData("NEW_PASSPORT_123");
         assertEquals(0, patientIdentityService.getAll().size());
 
@@ -122,7 +123,7 @@ public class PatientIdentityServiceTest extends BaseWebContextSensitiveTest {
         assertNotNull(patientIdentity);
 
         // Update the patient identity
-        String updatedData = "UPDATED_ID_12345";
+        String updatedData = "UPDATED_DATA";
         patientIdentity.setIdentityData(updatedData);
         patientIdentityService.update(patientIdentity);
 
