379ca124e213f80c540df468bf493dcd17404229
Adarsh Samani <samaniadarsh@gmail.com>
2025-03-24 14:47:54 +0530

    Apply format

--- src/test/java/org/openelisglobal/AppTestConfig.java
+++ src/test/java/org/openelisglobal/AppTestConfig.java
@@ -96,12 +96,6 @@ public class AppTestConfig implements WebMvcConfigurer {
 
     @Bean()
     @Profile("test")
-    public PluginAnalyzerService pluginAnalyzerService() {
-        return mock(PluginAnalyzerService.class);
-    }
-
-    @Bean()
-    @Profile("test")
     public FhirPersistanceService fhirPesistence() {
         return mock(FhirPersistanceService.class);
     }
