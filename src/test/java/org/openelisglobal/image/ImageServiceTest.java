586aaa15c50a45a73b12c021f3e8f51e2019c921
Isabirye1515 <isabiryeelijah15@gmail.com>
2025-04-02 06:56:09 +0300

    adding more images tests

--- src/test/java/org/openelisglobal/image/ImageServiceTest.java
+++ src/test/java/org/openelisglobal/image/ImageServiceTest.java
@@ -8,10 +8,12 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.openelisglobal.BaseWebContextSensitiveTest;
+import org.openelisglobal.common.util.ConfigurationProperties;
 import org.openelisglobal.image.service.ImageService;
 import org.openelisglobal.image.valueholder.Image;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -127,4 +129,97 @@ public class ImageServiceTest extends BaseWebContextSensitiveTest {
 
     }
 
+    @Test
+    public void getCount() {
+        int count = imageService.getCount();
+        assertEquals(4, count);
+    }
+
+    @Test
+    public void getCountLike() {
+        int count = imageService.getCountLike("description", "headerLeftImage");
+        assertEquals(1, count);
+    }
+
+    @Test
+    public void getAllOrdered_shouldReturnAllImagesOrdered() {
+        List<Image> images = imageService.getAllOrdered("id", false);
+        assertEquals(4, images.size());
+        assertEquals("headerLeftImage", images.get(0).getDescription());
+        assertEquals("headerRightImage", images.get(1).getDescription());
+        assertEquals("labDirectorSignature", images.get(2).getDescription());
+        assertEquals("Description4", images.get(3).getDescription());
+    }
+
+    @Test
+    public void getPage_shouldReturnPageOfImages() {
+        List<Image> images = imageService.getPage(1);
+        int expectedImages = Integer
+                .parseInt(ConfigurationProperties.getInstance().getPropertyValue("page.defaultPageSize"));
+        assertTrue(images.size() <= expectedImages);
+
+    }
+
+    @Test
+    public void getAllMarchingOrdered_shouldReturnAllImagesOrdered() {
+        List<Image> images = imageService.getAllMatchingOrdered("description", "headerLeftImage", "id", false);
+        assertEquals(1, images.size());
+        assertEquals("headerLeftImage", images.get(0).getDescription());
+    }
+
+    @Test
+    public void getAllMatchingOrderedPaged_shouldReturnPageOfImages() {
+        List<Image> images = imageService.getMatchingOrderedPage("description", "headerLeftImage", "id", false, 1);
+        int expectedImages = Integer
+                .parseInt(ConfigurationProperties.getInstance().getPropertyValue("page.defaultPageSize"));
+        assertTrue(images.size() <= expectedImages);
+    }
+
+    @Test
+    public void getNext_shouldReturnNextImage() {
+        Image nextImage = imageService.getNext("1");
+        assertEquals("2", nextImage.getId());
+    }
+
+    @Test
+    public void getPrevious_shouldReturnPreviousImage() {
+        Image previousImage = imageService.getPrevious("2");
+        assertEquals("1", previousImage.getId());
+    }
+
+    @Test
+    public void get_shouldReturnImageGiveId() {
+        Image image = imageService.get("1");
+        assertEquals("headerLeftImage", image.getDescription());
+    }
+
+    @Test
+    public void getCountMatching_shouldReturnCountOfImages() {
+        int count = imageService.getCountMatching("description", "headerLeftImage");
+        assertEquals(1, count);
+    }
+
+    @Test
+    public void getAllMatching_givenMaps() {
+        Map<String, Object> map = Map.of("description", "headerLeftImage");
+        List<Image> images = imageService.getAllMatching(map);
+        assertEquals(1, images.size());
+    }
+
+    @Test
+    public void getAllMatchingOrderedPaged_givenMaps() {
+        Map<String, Object> map = Map.of("description", "headerLeftImage");
+        List<Image> images = imageService.getMatchingOrderedPage(map, "id", false, 1);
+        int expectedImages = Integer
+                .parseInt(ConfigurationProperties.getInstance().getPropertyValue("page.defaultPageSize"));
+        assertTrue(images.size() <= expectedImages);
+    }
+
+    @Test
+    public void getAllMatchingOrdered_givenMaps() {
+        Map<String, Object> map = Map.of("description", "headerLeftImage");
+        List<Image> images = imageService.getAllMatchingOrdered(map, "id", false);
+        assertEquals(1, images.size());
+        assertEquals("headerLeftImage", images.get(0).getDescription());
+    }
 }
