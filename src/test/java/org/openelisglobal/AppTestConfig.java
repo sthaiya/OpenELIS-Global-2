03580044484b345dd55f3c73fe00fe4966937c04
Isabirye1515 <isabiryeelijah15@gmail.com>
2025-04-09 19:10:47 +0300

    Resoving the conflict

--- src/test/java/org/openelisglobal/AppTestConfig.java
+++ src/test/java/org/openelisglobal/AppTestConfig.java
@@ -71,7 +71,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "org.openelisglobal.analyzerimport", "org.openelisglobal.analyzer", "org.openelisglobal.testanalyte",
         "org.openelisglobal.observationhistory", "org.openelisglobal.systemusersection",
         "org.openelisglobal.typeofsample", "org.openelisglobal.siteinformation", "org.openelisglobal.config",
-        "org.openelisglobal.image", "org.openelisglobal.testresult" }, excludeFilters = {
+        "org.openelisglobal.image", "org.openelisglobal.testresult", "org.openelisglobal.barcode.service",
+        "org.openelisglobal.barcode.daoimpl" }, excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.patient.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.organization.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.sample.controller.*"),
